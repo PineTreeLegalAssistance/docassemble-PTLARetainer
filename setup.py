f75ea550a0ef77332b3b617b2f54d60dfaccc1f9
Jack Haycock <jhaycock@ptla.org>
2025-12-03 08:55:14 -0500

    Retainer selection code working - next steps patching up email relay to match

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -50,12 +50,10 @@ setup(name='docassemble.PTLARetainer',
       long_description_content_type='text/markdown',
       author='Jack Haycock',
       author_email='jhaycock@ptla.org',
-      license='The MIT License (MIT)',
+      license='MIT',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/PTLARetainer/', package='docassemble.PTLARetainer'),
      )
-
